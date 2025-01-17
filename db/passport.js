11d539b3190d5fd13ccf50af4c4673c630445f9d
Naveenpl1081 <123857457+Naveenpl1081@users.noreply.github.com>
2025-01-17 15:51:51 +0530

    gAuth

--- db/passport.js
+++ db/passport.js
@@ -8,7 +8,7 @@ const env=require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'/auth/google/callback'
+    callbackURL:'/auth/google/callback' || 'https://www.voltcraft.shop/auth/google/callback'
 },
 
 async (accesToken,refreshToken,profile,done)=>{
