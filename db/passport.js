c923fe2db6a06306f43a4677edfdf7781598277b
Naveenpl1081 <123857457+Naveenpl1081@users.noreply.github.com>
2025-06-16 23:22:45 +0530

    done

--- db/passport.js
+++ db/passport.js
@@ -8,7 +8,7 @@ const env=require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'http://localhost:3001/auth/google/callback' || '/auth/google/callback' 
+    callbackURL:process.env.CALLBACKURL || '/auth/google/callback' 
 },
 
 async (accesToken,refreshToken,profile,done)=>{
